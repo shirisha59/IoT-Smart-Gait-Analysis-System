d8faf74258eff97bae4f21eb7d395a8ae8606473
nikhila <mademnikhi@gmail.com>
2026-04-27 14:03:32 +0530

    Updated app for Render deployment

--- project/app.py
+++ project/app.py
@@ -451,7 +451,8 @@ def logout():
     return redirect("/")
 
 # =========================
-# RUN
-# =========================
+# RUN (UPDATED FOR RENDER)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
